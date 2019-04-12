6ad8c9202e70b02544431db2bf375a0b9c33a08b
Uranop <andreylucasvg@gmail.com>
2019-04-12 12:07:46 -0300

    Modificando o layout

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,7 +1,46 @@
 <template>
-    <div>
-        <input type="text" placeholder="Página de inicio">
-        <v-btn color="success">Success</v-btn>
-
+  <div class="img">
+      <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Astronomia</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          
+          <v-btn flat>Noticias</v-btn>
+          <v-btn flat>Página de Login</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      
+      <v-carousel>
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :key="i"
+            :src="item.src"
+          ></v-carousel-item>
+        </v-carousel>
     </div>
+
 </template>
+<script>
+    export default {
+      data () {
+        return {
+          items: [
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+            },
+            {
+              src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+            }
+          ]
+        }
+      }
+    }
+  </script>
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,113 @@
 <template>
-    <div>
-       <h1> login </h1>
-       
-    </div>
+        <div class="img">
+            <v-toolbar color="">
+              <v-toolbar-side-icon></v-toolbar-side-icon>
+              <v-toolbar-title>Página de Login</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items class="hidden-sm-and-down">
+                
+                <v-btn flat>Noticias</v-btn>
+                <v-btn flat> Página de Inicial</v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+    <v-layout align-center justify-center row fill-height mt-5 >
+      <v-card color="grey lighten-1 " class="elevation-5 pa-4">
+      <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      color="pink"
+    >
 
-    
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="senha"
+        type="password"
+        label="Senha"
+        required
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="checkbox"
+        label="Salvar senha?"
+        required
+      ></v-checkbox>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        @click="validate"
+      >
+        Voltar para Página inicial
+      </v-btn>
+
+      <v-btn
+        color="blue"
+        @click="resetValidation"
+      >
+        Confirmar Login
+      </v-btn>
+  </v-form>
+</v-card>
+</v-layout>
+</div>
 </template>
+
+<script> 
+export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: false
+    }),
+
+    methods: {
+      validate () {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      }
+    }
+  }
+</script>
+<style>
+  .teste{
+    background-color: red !important;
+  }
+</style>
